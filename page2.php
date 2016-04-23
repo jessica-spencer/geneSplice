53aef729c1007c14b40342208b9c5714baba2ea8
jessica-spencer <jspence4@conncoll.edu>
2016-04-23 15:16:41 -0400

    using strings instead of reading in file b/c backend not set up yet

--- page2.php
+++ page2.php
@@ -2,21 +2,52 @@
 <h1> Processing... </h1>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script>
- 
-
+ var file;
+var file = "# Count File for trans0_track1\
+>trans0_track1 Cardinality27\
+01 13674\
+0i 34\
+0j 1\
+12 13665\
+1i 43\
+20 13618\
+2i 58\
+2j 32\
+AC 50363\
+AI 263\
+Aj 102\
+BA 50560\
+BI 168\
+CB 50596\
+CI 131\
+IA 168\
+IB 131\
+IC 263\
+II 197322\
+i0 58\
+i1 34\
+i2 43\
+ii 74470\
+j0 33\
+jB 1\
+jC 101\
+jj 14289158";
 //read in with jQuery
-var dataA = jQuery.get('II.trans0_track1', function(data) {
-  // alert(data);
+jQuery.get('II.trans0_track1', function(data) {
+    alert(data + "hsldkfjlsdkjflskfjsldfjdslfkjsflk");
+    file = str(data);
    //process text file line by line
   // $('#div').html(data.replace('n',''));
 });  
-alert(dataA);
-    
+//document.getElementById("yo").innerHTML = dataA;
+alert(file);  
     
     
     
 </script> 
-    
+    <div id="yo">
+    hey. been dying, to meet you. 
+    </div>
 <!--
 mysqli_connect("localhost");
 error_log("ON THE SERVER BITCH _________________________________");
