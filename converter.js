0f503602f9c9900889ab9222a7a0ed7910cee4c7
jessica-spencer <jspence4@conncoll.edu>
2016-04-23 14:24:16 -0400

    beginning converter js

---
+++ converter.js
@@ -0,0 +1,1 @@
+//this will be the converter from the output files to jSon
---
+++ page2.php
@@ -0,0 +1,14 @@
+<html>
+<h1> Processing... </h1>
+<?php
+mysqli_connect("localhost");
+error_log("ON THE SERVER BITCH _________________________________");
+//system ( '../AGP_things/AGP/Worm_Starter.pl.sh', $result);
+ //shell_exec('perl ../AGP_things/AGP/Worm_Starter.pl.sh');
+error_log("OFF THAT SURVUR AGAYN MOM ___________________________");
+//system ( "nohup ../AGP_things/AGP/Worm_Starter.pl.sh > /dev/null 2>&1 &" , $result);
+echo('hello');
+//echo($result);
+
+?>
+</html>
