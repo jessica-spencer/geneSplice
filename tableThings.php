3803fc402746bc2e79d017d39a976db2915062a5
surendralama <surendralama08@gmail.com>
2016-05-02 19:00:49 -0400

    Added files via upload

--- tableThings.php
+++ tableThings.php
@@ -1,6 +1,5 @@
 <?
 include("initDB.php");
-
 function connectServer () {
     $servername = "localhost";
     // Create connection
@@ -12,34 +11,60 @@ function connectServer () {
     } 
     return $conn;
 }
-
-
-function insert($filename, $conn) {
+function insert($filename) {
     $date = date("Y/m/d");
     
     $request = "INSERT INTO fastaFiles(id," . $filename . ", outputFiles, reg_date) VALUES               (0,'jessica',';sakldjf;alskdjf;alsdkjf;asldkfjasd;lfkj','" . $date . "'";
-    $results = $conn->query($request);   
-}
+    $results = $conn->query($request);  
 
-function isFile($filename, $conn){
+    if ($conn->query($request) === TRUE) {
+    echo "New record created successfully";
+    } else {
+    echo  $sql . "<br>" . $conn->error;
+    }
+ }
+
+function isFile($filename){
     $query = "SELECT * FROM `fastaFiles` WHERE fileName ='" . $filename . "'";
     $results = $conn->query($request);
     return($results);
     
+    if ($conn->query($query) === TRUE) {
+    echo "New record created successfully";
+    } else {
+    return FALSE;
+    }
 }
     
-function getFile($filename, $conn) {
+function getFile($filename) {
     $query = "SELECT * FROM `fastaFiles` WHERE fileName ='" . $filename . "'";
     $results = $conn->query($request);
     return($results);
-}
 
+    if ($conn->query($query) === TRUE) {
+    $last_id = $conn->insert($filename);
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
+    } else {
+    echo "Error: there is not file with that name" . $sql . "<br>" . $conn->error;
+    }
+}
 /*function emailInsert($email,$fileName) {
     $request = "INSERT INTO emailTable (email, fileName) VALUES (" . $email .  "," . $fileName . ")";
     $results = $conn->query($request);
     
 }
-
 function getEmail($)*/
+
+function run(){
+    if($conn->insert()){
+        $results.insert()
+    }
+    if ($conn->isFile()) {
+        $results.isFile()
+    }
+    if ($conn->getFile) {
+        $results.getFile()
+    }
+}
 
 ?>
