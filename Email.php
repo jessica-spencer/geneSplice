e9b0b83012f653c6e902c9f97d9262c8d8a0b650
surendralama <surendralama08@gmail.com>
2016-05-02 19:43:05 -0400

    Added files via upload

---
+++ Email.php
@@ -0,0 +1,23 @@
+<?php
+
+$mailacc = "slama1@conncoll.edu";
+
+$subject = "Gene Output";
+
+$message = "Here is your output file:
+<html>
+<head>
+  <title>Test Mail</title>
+</head>
+<body>
+    <p><a href='http://www.w3schools.com/html/html_links.asp'>Open Link</a></p>
+</body>
+</html>
+";
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: Noreply <noreply@example.com>' . "\r\n";
+
+$mail = mail($mailacc, $subject, $message, $headers);
+?>
