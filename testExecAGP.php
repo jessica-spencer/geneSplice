483f89af584df83fff6fc734d1e81c5b3d619e9e
jessica-spencer <jspence4@conncoll.edu>
2016-05-05 16:40:27 -0400

    working on testExecAGP.php

--- testExecAGP.php
+++ testExecAGP.php
@@ -1,7 +1,23 @@
 <? 
 // trying with the server side execution of the AGP
-echo("")
-exec ("perl ../AGP/Worm_Starter.pl.sh");
-echo("yup")
+//include("initDB.php");
+echo("here we go");
+$servername = "localhost";
+$username = "";
+$password = "";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+$returnVal = error_log("hello there error log HIIIIII");
+error_log("\n" . $returnVal);
+echo("START THE COOOOOODE ______________________");
+$error = shell_exec("perl ..AGP_things/AGP/GFF_Collect_fromEnsembl3.pl");
+echo($error);
+error_log("yup")
 ?>
