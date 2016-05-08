d63a491cd3aec7019336b6b647c4de05a2b2f8b5
surendralama <surendralama08@gmail.com>
2016-05-08 13:53:28 -0400

    Added files via upload

--- Email.php
+++ Email.php
@@ -1,11 +1,10 @@
 <?php
-
 function sendMail($address,$id) {
     $mailacc = $address;
-
     $subject = "Gene Output";
-
-    $message = "Here is your output file:
+    $message = "Dear User, <br>
+	<br> We are the Metalogos Alt Splice team. Here is the link to your input file.<br>
+	<br>Thanks, for using our website.<br> 
     <html>
     <head>
       <title>Test Mail</title>
@@ -16,11 +15,9 @@ function sendMail($address,$id) {
     </body>
     </html>
     ";
-
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $headers .= 'From: Metalogos Alt Splice <noreply@example.com>' . "\r\n";
-
     $mail = mail($mailacc, $subject, $message, $headers);
 }
 ?>
